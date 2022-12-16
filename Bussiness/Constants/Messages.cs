af7718602eabfd7b9dbb45e0ede4575861a6361d
Sakir Ayitki <ayitkisakir1@gmail.com>
2022-12-17 01:33:52 +0300

    Add project files.

---
+++ Bussiness/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added Succesfully!";
+        public static string ProductNameInvalid = "Product Name Is Invalid!";
+        public static string MaintenanceTime = "System In Maintenance!";
+        public static string ProductsListed = "Products Listed!";
+
+    }
+}
---
+++ de/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bussiness.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            // CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails().Data)
+            {
+                Console.WriteLine("Product:" + product.ProductName + "\nCategory:" + product.CategoryName + "\nUnits In Stock:" + product.UnitsInStock +" \n\n");
+            }
+        }
+    }
+}
